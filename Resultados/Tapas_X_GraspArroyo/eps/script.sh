5ec7800891ad61eeb4e2898a738f2e4c1a3e8f32
Islame Felipe Da costa Fernandes <islame-felipe.da-costa-fernandes@etu.unice.fr>
2017-06-16 13:20:27 -0300

    experimentos e testes estatisticos

--- Resultados/Tapas_X_GraspArroyo/eps/script.sh
+++ Resultados/Tapas_X_GraspArroyo/eps/script.sh
@@ -4,7 +4,6 @@ for p in 20 30 50
 do
 	for i in 1 2 3 4 5
 	do
-		#verficiar se estah tudo certo com o calculo do epsilon
 		cat ../../../Tapas_rocha/Teste/grasp/$p.grasp$i/$p.grasp$i.PF.log ../../../GraspOficial/Teste/grasp/$p.grasp$i/$p.grasp$i.PF.log > inputFilter.in
 		../../../../../Epsilon/PISA/tools_c_source/filter ../../../../../Epsilon/PISA/tools_c_source/filter_param.txt inputFilter.in reference.out
 		../../../../../Epsilon/PISA/indicators_c_source/eps_ind ../../../../../Epsilon/PISA/indicators_c_source/eps_ind_param.txt ../../../Tapas_rocha/Teste/grasp/$p.grasp$i/$p.grasp$i.PF.log reference.out $p.grasp$i/resultadoTapasEps.out
--- Resultados/Tapas_X_memeticoRocha/hypervolume/script.sh
+++ Resultados/Tapas_X_memeticoRocha/hypervolume/script.sh
@@ -14,24 +14,15 @@ for p in 50 100 200 300 400 500 600 700 800 900 #1000
 do
 	for i in 1 2 3 4 5
 	do
-		cat ../../../Tapas_rocha/Teste/INST1/correlated/$p.corr$i/$p.PF$i.log ../../../memeticRocha2006/algunsTestesRocha/Teste/INST1/correlated/$p.corr$i/$p.PF$i.log > inputFilter.in
+
+		cat ../../../Tapas_rocha/Teste/INST1/concave/$p.conc$i/$p.PF$i.log ../../../memeticRocha2006/algunsTestesRocha/Teste/INST1/concave/$p.conc$i/$p.PF$i.log > inputFilter.in
 		../../../../../Epsilon/PISA/tools_c_source/filter ../../../../../Epsilon/PISA/tools_c_source/filter_param.txt inputFilter.in reference.out
-		../../../../../Epsilon/PISA/indicators_c_source/hyp_ind ../../../../../Epsilon/PISA/indicators_c_source/hyp_ind_param.txt ../../../Tapas_rocha/Teste/INST1/correlated/$p.corr$i/$p.PF$i.log reference.out correlated/$p.corr$i/resultadoHypTapas.out
-		../../../../../Epsilon/PISA/indicators_c_source/hyp_ind ../../../../../Epsilon/PISA/indicators_c_source/hyp_ind_param.txt ../../../memeticRocha2006/algunsTestesRocha/Teste/INST1/correlated/$p.corr$i/$p.PF$i.log reference.out correlated/$p.corr$i/resultadoHypMemeticoRocha.out
+		../../../../../Epsilon/PISA/indicators_c_source/hyp_ind ../../../../../Epsilon/PISA/indicators_c_source/hyp_ind_param.txt ../../../Tapas_rocha/Teste/INST1/concave/$p.conc$i/$p.PF$i.log reference.out concave/$p.conc$i/resultadoHypTapas.out
+		../../../../../Epsilon/PISA/indicators_c_source/hyp_ind ../../../../../Epsilon/PISA/indicators_c_source/hyp_ind_param.txt ../../../memeticRocha2006/algunsTestesRocha/Teste/INST1/concave/$p.conc$i/$p.PF$i.log reference.out concave/$p.conc$i/resultadoHypMemeticoRocha.out
 		
-		echo "$(cat correlated/$p.corr$i/resultadoHypTapas.out)\n\n$(cat correlated/$p.corr$i/resultadoHypMemeticoRocha.out)" >> correlated/$p.corr$i/uniHypervolume.out #primeiro tapas
+		echo "$(cat concave/$p.conc$i/resultadoHypTapas.out)\n\n$(cat concave/$p.conc$i/resultadoHypMemeticoRocha.out)" >> concave/$p.conc$i/uniHypervolume.out #primeiro tapas
 
-		../../../../../Epsilon/PISA/statistics_c_source/mann-whit correlated/$p.corr$i/uniHypervolume.out ../../../../../Epsilon/PISA/statistics_c_source/param.txt correlated/$p.corr$i/mann_whit.out
-		
-
-		cat ../../../Tapas_rocha/Teste/INST1/anticorrelated/$p.anticorr$i/$p.PF$i.log ../../../memeticRocha2006/algunsTestesRocha/Teste/INST1/anticorrelated/$p.anticorr$i/$p.PF$i.log > inputFilter.in
-		../../../../../Epsilon/PISA/tools_c_source/filter ../../../../../Epsilon/PISA/tools_c_source/filter_param.txt inputFilter.in reference.out
-		../../../../../Epsilon/PISA/indicators_c_source/hyp_ind ../../../../../Epsilon/PISA/indicators_c_source/hyp_ind_param.txt ../../../Tapas_rocha/Teste/INST1/anticorrelated/$p.anticorr$i/$p.PF$i.log reference.out anticorrelated/$p.anticorr$i/resultadoHypTapas.out
-		../../../../../Epsilon/PISA/indicators_c_source/hyp_ind ../../../../../Epsilon/PISA/indicators_c_source/hyp_ind_param.txt ../../../memeticRocha2006/algunsTestesRocha/Teste/INST1/anticorrelated/$p.anticorr$i/$p.PF$i.log reference.out anticorrelated/$p.anticorr$i/resultadoHypMemeticoRocha.out
-		
-		echo "$(cat anticorrelated/$p.anticorr$i/resultadoHypTapas.out)\n\n$(cat anticorrelated/$p.anticorr$i/resultadoHypMemeticoRocha.out)" >> anticorrelated/$p.anticorr$i/uniHypervolume.out #primeiro tapas
-
-		../../../../../Epsilon/PISA/statistics_c_source/mann-whit anticorrelated/$p.anticorr$i/uniHypervolume.out ../../../../../Epsilon/PISA/statistics_c_source/param.txt anticorrelated/$p.anticorr$i/mann_whit.out
+		../../../../../Epsilon/PISA/statistics_c_source/mann-whit concave/$p.conc$i/uniHypervolume.out ../../../../../Epsilon/PISA/statistics_c_source/param.txt concave/$p.conc$i/mann_whit.out
 		
 	done
 done
