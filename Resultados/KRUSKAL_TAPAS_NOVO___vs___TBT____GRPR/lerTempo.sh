12e45bdf9050ac0eeff436f2bcd8495685e06ad1
Islame Felipe Da costa Fernandes <islame-felipe.da-costa-fernandes@etu.unice.fr>
2018-03-27 14:19:02 -0300

    recuperacao dos resultados individuais do TBTv1

--- Resultados/KRUSKAL_TAPAS_NOVO___vs___TBT____GRPR/lerTempo.sh
+++ Resultados/KRUSKAL_TAPAS_NOVO___vs___TBT____GRPR/lerTempo.sh
@@ -5,22 +5,22 @@ do
 	do
 		# corr
 		./lerTempo ../../Grasp_PR/Testes/INST1/correlated/"$p".corr"$i"/tempo_"$p".corr"$i".out 
-		./lerTempo ../../Trans1_2obj_hibrid_VVersion1/Testes/INST1/correlated/"$p".corr"$i"/tempo_"$p".corr"$i".out
 		./lerTempo ../../Tapas_rocha_NOVO/Testes/INST1/correlated/"$p".corr"$i"/tempo_"$p".corr"$i".out
-	
+		./lerTempo ../../Trans1_2obj_hibrid_VVersion1/Testes/INST1/correlated/"$p".corr"$i"/tempo_"$p".corr"$i".out
+		
 		
 
 		# anticorr
 		./lerTempo ../../Grasp_PR/Testes/INST1/anticorrelated/"$p".anticorr"$i"/tempo_"$p".anticorr"$i".out 
-		./lerTempo ../../Trans1_2obj_hibrid_VVersion1/Testes/INST1/anticorrelated/"$p".anticorr"$i"/tempo_"$p".anticorr"$i".out
 		./lerTempo ../../Tapas_rocha_NOVO/Testes/INST1/anticorrelated/"$p".anticorr"$i"/tempo_"$p".anticorr"$i".out
-	
+		./lerTempo ../../Trans1_2obj_hibrid_VVersion1/Testes/INST1/anticorrelated/"$p".anticorr"$i"/tempo_"$p".anticorr"$i".out
+		
 
 		# conc
 		./lerTempo ../../Grasp_PR/Testes/INST1/concave/"$p".conc"$i"/tempo_"$p".conc"$i".out 
-		./lerTempo ../../Trans1_2obj_hibrid_VVersion1/Testes/INST1/concave/"$p".conc"$i"/tempo_"$p".conc"$i".out
 		./lerTempo ../../Tapas_rocha_NOVO/Testes/INST1/concave/"$p".conc"$i"/tempo_"$p".conc"$i".out
-	
+		./lerTempo ../../Trans1_2obj_hibrid_VVersion1/Testes/INST1/concave/"$p".conc"$i"/tempo_"$p".conc"$i".out
+		
 
 		echo ""
 
