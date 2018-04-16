4c860b11d7d5a1b907dd7b936cf99158f9b1208c
Islame Felipe Da costa Fernandes <islame-felipe.da-costa-fernandes@etu.unice.fr>
2018-04-16 15:04:38 -0300

    resultados TBT quantidade de avaliaçoes

--- Trans1_2obj_hibrid_VVersion1_ITERACOES/scriptAnticorr.sh
+++ Trans1_2obj_hibrid_VVersion1_ITERACOES/scriptAnticorr.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 cd Testes/INST1/
 
-			k=900
-			i=3
-			c=1
-	        while [ $c -le 30 ]; 
-	        do
-				ran=$(( $RANDOM % 9384983948 ));
-				./trans_"$k" $ran 270000 correlated/"$k".corr"$i"/pareto_"$k".corr$"i".out correlated/"$k".corr"$i"/tempo_"$k".corr$"i".out < ../../../InstanciasMestrado/INST1/correlated/"$k".corr"$i".in > correlated/"$k".corr"$i"/"$k".corr"$i"_"$c".out  
-				c=$((c+1))
-			done
-
 for k in 1000 
 do
-	for i in 1 2 3 
+	for i in 3 
 	do
-			c=1
+			c=27
 	        while [ $c -le 30 ]; 
 	        do
 				ran=$(( $RANDOM % 9384983948 ));
